7e46578fe8ecb2dee8377305f6563353ce517893
alvarohc777 <aherrada@uninorte.edu.co>
2023-04-12 01:03:30 -0500

    Add Get endpoints

--- utraffic/CRUD/api.py
+++ utraffic/CRUD/api.py
@@ -1,6 +1,31 @@
 from ninja import NinjaAPI
+from typing import List, Optional
+
+from CRUD.models import Cliente, Factura, PagoFactura
+from CRUD.schema import ClienteSchema, FacturaSchema, PagoFacturaSchema, NotFoundSchema
+
 
 api = NinjaAPI()
+
+
+@api.get("/cliente", response=List[ClienteSchema])
+def clientes(request, nombre: Optional[str] = None):
+    if nombre:
+        return Cliente.objects.filter(nombre__icontains=nombre)
+    return Cliente.objects.all()
+
+
+@api.get("/cliente/{cliente_id}", response={200: ClienteSchema, 404: NotFoundSchema})
+def cliente(request, cliente_id: int):
+    try:
+        cliente = Cliente.objects.get(pk=cliente_id)
+        return 200, cliente
+    except Cliente.DoesNotExist as e:
+        return 404, {"message": "El cliente no existe"}
+
+
+# def create_cliente(request, payload: ClienteSchema):
+
 
 @api.get("/hello")
 def hello(request):
--- utraffic/CRUD/models.py
+++ utraffic/CRUD/models.py
@@ -21,7 +21,7 @@ class Factura(models.Model):
     comentarios = models.TextField()
     saldo = models.DecimalField(max_digits=15, decimal_places=2, default=0)
 
-    # updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.pk} - {self.id_cliente}"
