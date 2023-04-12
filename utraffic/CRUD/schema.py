66caf03ddc89952b04857983c5b47917f8e9ed06
alvarohc777 <aherrada@uninorte.edu.co>
2023-04-12 04:01:30 -0500

    Refactor Pydantic models Schemas

--- utraffic/CRUD/schema.py
+++ utraffic/CRUD/schema.py
@@ -1,27 +1,48 @@
 from datetime import date
-from ninja import Schema
+from ninja import Schema, ModelSchema
+from ninja.orm import create_schema
+from CRUD.models import Cliente, Factura, PagoFactura
 
 
-class ClienteSchema(Schema):
-    id: int
-    nombre: str
-    direccion: str
-    telefono: str
+ClienteBase = create_schema(Cliente, fields=["id", "nombre", "direccion", "telefono"])
+ClienteCreate = create_schema(Cliente, fields=["nombre", "direccion", "telefono"])
+
+# class ClienteSchema(ModelSchema):
+#     class Config:
+#         model = Cliente
+#         model_fields = ["nombre", "direccion", "telefono"]
 
 
-class FacturaSchema(Schema):
-    id_cliente: int
-    fecha: date
-    valor: float
-    comentarios: str
-    saldo: float
+# class ClienteSchema(Schema):
+#     nombre: str
+#     direccion: str
+#     telefono: str
 
 
-class PagoFacturaSchema(Schema):
-    id_factura: int
-    fecha: date
-    valor: float
-    comentarios: str
+# class FacturaBase(ModelSchema):
+#     class Config:
+#         model = Factura
+#         model_fields = ["id", "cliente", "fecha", "valor", "comentarios", "saldo"]
+
+
+# class FacturaCreate(ModelSchema):
+#     class Config:
+#         model = Factura
+#         model_fields = ["cliente", "fecha", "valor", "comentarios", "saldo"]
+
+
+FacturaBase = create_schema(
+    Factura, fields=["id", "cliente", "fecha", "valor", "comentarios", "saldo"]
+)
+FacturaCreate = create_schema(
+    Factura, fields=["cliente", "fecha", "valor", "comentarios", "saldo"]
+)
+PagoFacturaBase = create_schema(
+    PagoFactura, fields=["id", "factura", "fecha", "valor", "comentarios"]
+)
+PagoFacturaCreate = create_schema(
+    PagoFactura, fields=["factura", "fecha", "valor", "comentarios"]
+)
 
 
 class NotFoundSchema(Schema):
