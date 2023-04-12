d78fad1103d85dd3c454f8943ccd878ada9c0068
alvarohc777 <aherrada@uninorte.edu.co>
2023-04-12 03:56:54 -0500

    Fix models
    Remove id_ from names, not required because django's ORM automatically recognizes and adds this info

--- utraffic/CRUD/models.py
+++ utraffic/CRUD/models.py
@@ -15,7 +15,7 @@ class Cliente(models.Model):
 
 
 class Factura(models.Model):
-    id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     fecha = models.DateField(auto_now_add=True)
     valor = models.DecimalField(max_digits=15, decimal_places=2)
     comentarios = models.TextField()
@@ -24,7 +24,7 @@ class Factura(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.pk} - {self.id_cliente}"
+        return f"{self.pk} - {self.cliente}"
 
     def save(self, *args, **kwargs):
         if not self.id:
@@ -34,7 +34,7 @@ class Factura(models.Model):
 
 
 class PagoFactura(models.Model):
-    id_factura = models.ForeignKey(Factura, on_delete=models.CASCADE)
+    factura = models.ForeignKey(Factura, on_delete=models.CASCADE)
     fecha = models.DateField(auto_now_add=True)
     valor = models.DecimalField(max_digits=15, decimal_places=2)
     comentarios = models.TextField()
@@ -42,10 +42,10 @@ class PagoFactura(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
-        return f"{self.id_factura} - {self.fecha}"
+        return f"{self.factura} - {self.fecha}"
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        self.id_factura.saldo -= self.valor
-        self.id_factura.save(update_fields=["saldo"])
+        self.factura.saldo -= self.valor
+        self.factura.save(update_fields=["saldo"])
         # self.id_factura.save
