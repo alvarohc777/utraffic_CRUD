c7d9a78e53caf07774a467cc9acc9b7f87e0eef3
alvarohc777 <aherrada@uninorte.edu.co>
2023-04-12 10:31:03 -0500

    Update Database settings
    a .env file must be created with:
    SECRET_KEY=django-insecure-728k0bs%91ogitsp%aa_ji@2fmtwpdk7r1na#*$%l2+%)7tnpo3
    DB_NAME=your_db_name
    DB_USER=your_db_user
    DB_PASSWORD=your_password
    DB_HOST=localhost
    DB_PORT=5432

--- utraffic/utraffic/settings.py
+++ utraffic/utraffic/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+SECRET_KEY = env("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +37,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "CRUD",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,7 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "ninja",
-    "CRUD",
+    
 ]
 
 MIDDLEWARE = [
@@ -77,8 +84,12 @@ WSGI_APPLICATION = "utraffic.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
